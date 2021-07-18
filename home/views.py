df2a89c25762d762f99d8d76c020c36973c9fa51
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2021-07-18 22:25:37 +0530

    Added necessary code to enable get requests

--- home/views.py
+++ home/views.py
@@ -1,7 +1,24 @@
+from os import stat
 from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 
-
 def index(request):
     return render(request, 'index.html')
+
+@csrf_exempt
+def ajax(request):
+    status = "error"
+    if(request.method == "POST"):
+        body_raw = request.body
+        json_body = json.loads(body_raw)
+        status = "OK"
+    return JsonResponse({"status":status, "request val": json_body})
+
+@csrf_exempt
+def ajax_GET(request):
+    raise PermissionDenied
