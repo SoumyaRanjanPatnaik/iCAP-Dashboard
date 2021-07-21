efe7de1d702929e2ffdb157017dd0058d48e9076
adi081219 <adityashakar98@gmail.com>
2021-07-22 00:53:45 +0530

    models updated

---
+++ home/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Woker(models.Model):
+   id = models.IntegerField
+   curr_bpm = models.FloatField
+   height = models.FloatField
+   avg_bpm = models.FloatField
+   temperature = models.FloatField
+   name = models.CharField
+   location = models.CharField
+   date = models.DateField(auto_now=False, auto_now_add=False)
+   time = models.TimeField( auto_now=False, auto_now_add=False)
+   
+
+
+
+# Create your models here.
