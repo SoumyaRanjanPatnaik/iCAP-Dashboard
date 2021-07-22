9e61616c231e94caab3e2a526d1adec3e1bc14db
Bhupendra <iiitianbhupendra@gmail.com>
2021-07-22 16:28:51 +0530

    Worker class and its propers are added and registered to get a table in the django admin pannel

--- home/models.py
+++ home/models.py
@@ -3,17 +3,16 @@ from django.db import models
 # Create your models here.
 
 
-class Woker(models.Model):
-    # id = models.AutoField()
+class Worker(models.Model):
     curr_bpm = models.TextField(max_length=50)
-    # height = models.FloatField(max_length=50)
-    # avg_bpm = models.FloatField(max_length=50)
-    # temperature = models.FloatField(max_length=50)
-    # name = models.CharField(max_length=50)
-    # location = models.CharField(max_length=50)
-    # fall = models.BooleanField()
-    # date = models.DateField(auto_now=False, auto_now_add=False)
-    # time = models.TimeField( auto_now=False, auto_now_add=False)
+    height = models.FloatField(max_length=50)
+    avg_bpm = models.FloatField(max_length=50)
+    temperature = models.FloatField(max_length=50)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    fall = models.BooleanField()
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    time = models.TimeField( auto_now=False, auto_now_add=False)
 
-def __str__(self):
-    return self.curr_bpm
+    def __str__(self):
+        return self.name 
